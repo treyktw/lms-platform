3774c24cc0120b287dad9c0a512e8cd27b5a9109
treyktw <tonystrax@gmail.com>
2023-10-24 23:12:43 -0400

    layoutchanges

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ const AuthLayout = ({
   children: React.ReactNode
 }) => {
   return (
-    <div className="h-full flex items-center justify-center bg-gradient-to-r from-red-400 to-purple-500">
+    <div className="h-full flex items-center justify-center">
       {children}
     </div>
   );
