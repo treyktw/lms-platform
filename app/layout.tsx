ed3a7edb5562020b65af9ad5951681b75a34591b
treyktw <tonystrax@gmail.com>
2023-10-24 22:46:08 -0400

    theme change

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-  <ClerkProvider appearance={{baseTheme: neobrutalism}}>
+  <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
         <ConfettiProvider/>
